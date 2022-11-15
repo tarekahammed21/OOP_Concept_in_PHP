eaab5f46a28d3a82738faa5ad8a97aab44f8f246
Md Tarek Ahammed Raju <106856675+tarekahammed21@users.noreply.github.com>
2022-11-15 19:33:56 +0600

    Add files via upload

---
+++ OOP_CONCEPT/DYNAMIC/biodata.php
@@ -0,0 +1,39 @@
+<?php
+
+
+class ManBasicInfo{
+    public $name;
+    public $age;
+    public $gender;
+
+public function SetData(){
+
+
+
+}
+
+
+    public function DisplayData(){
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+class ManContactInfo{
+    public $mobile;
+    public $email;
+    public $address;
+}
+
+
+
+
+?>
